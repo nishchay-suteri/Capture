510c44207a9fea3d5ca9269d5d39e3bb4f57fae3
nishchay271095 <nishchay271095@gmail.com>
2020-12-02 21:15:38 +0530

    Add Styled Component to About Section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,28 +1,59 @@
 import React from "react";
 import home1 from "../img/home1.png";
 
+import styled from "styled-components";
+
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <About>
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>We Work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>your dreams</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>come true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contact Us for any photography</p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
-                <img src={home1} alt="Home Image" />
-            </div>
-        </div>
+            </Description>
+            <Image>
+                <img src={home1} alt="Home" />
+            </Image>
+        </About>
     );
 };
+
+// Styled Components
+const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+const Description = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2 {
+        font-weight: lighter;
+    }
+`;
+const Image = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img {
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`;
+const Hide = styled.div`
+    overflow: hidden;
+`;
 export default AboutSection;
