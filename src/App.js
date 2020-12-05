e7ed34918ed73687ce73767cb66d236df5e521ac
nishchay271095 <nishchay271095@gmail.com>
2020-12-05 22:40:57 +0530

    Add Pages and Navigate using React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWorks from "./pages/OurWork";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
             <Nav />
-            <AboutUs />
+            <Switch>
+                <Route path="/" exact>
+                    <AboutUs />
+                </Route>
+                <Route path="/contact">
+                    <ContactUs />
+                </Route>
+                <Route path="/work">
+                    <OurWorks />
+                </Route>
+            </Switch>
         </div>
     );
 }
